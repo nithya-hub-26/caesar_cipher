541183b70b5542c33cff3a701ee5a0df2c9fc9ac
nithya-hub-26 <84989279+nithya-hub-26@users.noreply.github.com>
2021-06-18 17:11:57 +0530

    Create cipher_tool.py

---
+++ cipher_tool.py
@@ -0,0 +1,25 @@
+from string import ascii_letters
+#ascii_letters will import all the lower case and upper case alphabets
+
+
+def caesar_cipher(message, key):
+    s = ""
+    alphabet = ascii_letters + "0123456789"
+    for letter in message:
+        if letter in " ~`!@#$%^&*()+_-={}|[]\:;,./<>?":
+            s = s + letter
+        else:
+            s = s + alphabet[(alphabet.index(letter) + key) % 62]  # %62 is used to make the list cyclic
+    print(s)
+
+
+message = input("Enter the message which has to be encrypted or decrypted ")
+operation = input("Enter the operation whether to be encrypt or decrypt ")
+key = int(input("Enter the key value to shift the values "))
+if operation == "encrypt" or operation == "ENCRYPT" or operation == "E" or operation == "e":
+    caesar_cipher(message, key)
+elif operation == "decrypt" or operation == "DECRYPT" or operation == "D" or operation == "d":
+    key = - key
+    caesar_cipher(message, key)
+else:
+  print("enter the correct operation to be done")
